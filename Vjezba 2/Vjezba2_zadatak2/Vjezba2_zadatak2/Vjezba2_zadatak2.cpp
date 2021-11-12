2538afb533320ed08dc54d373655400cc52006b5
EDuc12 <93094258+EDuc12@users.noreply.github.com>
2021-11-12 13:16:56 +0100

    Update Vjezba2_zadatak2.cpp

--- Vjezba 2/Vjezba2_zadatak2/Vjezba2_zadatak2/Vjezba2_zadatak2.cpp
+++ Vjezba 2/Vjezba2_zadatak2/Vjezba2_zadatak2/Vjezba2_zadatak2.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
-void index(int *arr, int x, int &y)
+int &index(int* arr, int x, int y)
 {
+	
 	return arr[y];
 }
 void main()
 {
-	int x,y;
+	int x, y;
 	cout << "Size of array:";
 	cin >> x;
 	int* arr = new int[x];
 	cout << "Number index:";
 	cin >> y;
-	index(arr,x, y) +=1;
-	cout << y;
+	for (int i = 1; i <= x; i++)
+	{
+		cout << "Number:";
+		cin >> arr[i];
+	}
+	int &z= index(arr, x, y ) += 1;
+	cout << z ;
 }
