11a3b4f1858b393bcc8de29bd136adbe479d4291
EDuc12 <93094258+EDuc12@users.noreply.github.com>
2021-11-13 13:33:54 +0100

    Update Vjezba1_zadatak2.cpp

--- Vjezba 1/Vjezba1_zadatak2/Vjezba1_zadatak2/Vjezba1_zadatak2.cpp
+++ Vjezba 1/Vjezba1_zadatak2/Vjezba1_zadatak2/Vjezba1_zadatak2.cpp
@@ -8,119 +8,106 @@ typedef struct matrix
 } matrix;
 
 
-float ** input(matrix *mat)
+float** input(matrix* mat, float *x, float *y)
 {
-    float ** matrix1;
-    matrix1 = new float * [mat->rows];
-    for(int i = 0; i < mat->rows; i++)
+    float** matrix1;
+    cout << "input number of rows and collums";
+    cin >> mat->rows >> mat->cols;
+   *x = mat->rows;
+   *y = mat->cols;
+    matrix1 = new float* [mat->rows];
+    for (int i = 0; i < mat->rows; i++)
     {
-        matrix1[i] = new float [mat->cols];
-        for(int y=0; y < mat->cols; y++)
+        matrix1[i] = new float[mat->cols];
+        for (int y = 0; y < mat->cols; y++)
         {
             cout << "input numbers";
-            cin >> matrix1 [i] [y];
+            cin >> matrix1[i][y];
         }
     }
     return matrix1;
 }
-float** inputGen(matrix *mat)
+float** inputGen(matrix* mat)
 {
-    float ** matrix1;
-    matrix1 = new float * [mat->rows];
-    for(int i = 0; i < mat->rows; i++)
+    float** matrix1;
+    int a, b;
+    cout << "Typa a range of random numbers";
+    cin >> b >> a;
+    matrix1 = new float* [mat->rows];
+    for (int i = 0; i < mat->rows; i++)
     {
-        matrix1[i] = new float [mat->cols];
-        for(int y=0; y < mat->cols; y++)
+        matrix1[i] = new float[mat->cols];
+        for (int y = 0; y < mat->cols; y++)
         {
-            matrix1 [i] [y] = (float(rand()) / float((RAND_MAX)) * 10);
+            matrix1[i][y] = b + rand() % ((a + 1) - b);
         }
     }
     return matrix1;
 }
-float** count1(matrix *mat, float **mat1, float **mat2)
+float** count1(matrix* mat, float** mat1, float** mat2)
 {
     float** matrix3;
-    matrix3 = new float * [mat->rows];
-    if(mat->rows = mat1->rows && mat->cols = mat1->cols)
-    {
-        for(int i = 0; i < mat->rows; i++)
-    {
-        matrix3[i] = new float [mat->cols];
-        for(int y=0; y < mat->cols; y++)
+    matrix3 = new float* [mat->rows];
+        for (int i = 0; i < mat->rows; i++)
         {
-            matrix3 [i] [y] = mat1 [i] [y] + mat2 [i] [y];
+            matrix3[i] = new float[mat->cols];
+            for (int y = 0; y < mat->cols; y++)
+            {
+                matrix3[i][y] = mat1[i][y] + mat2[i][y];
+            }
         }
-    }
-    return matrix3;
-    }
-    else()
-    {
-        cout << "I cant do that math operation with different type of matrix."; 
-    }
+        return matrix3;
 }
-void output(matrix *mat, float **mat1)
+void output(matrix* mat, float** mat1)
 {
-    for(int i = 0; i < mat->rows; i++)
+    for (int i = 0; i < mat->rows; i++)
     {
-        for(int y=0; y < mat->cols; y++)
+        for (int y = 0; y < mat->cols; y++)
         {
-            cout << fixed << setprecision(4) << right << mat1 [i] [y] << " ";
+            cout << fixed << setprecision(4) << right << mat1[i][y] << " ";
         }
         puts(" ");
     }
 }
-float** minus1(matrix *mat, float **mat1, float **mat2)
+float** minus1(matrix* mat, float** mat1, float** mat2)
 {
     float** matrix3;
-    matrix3 = new float * [mat->rows];
-    if(mat->rows = mat1->rows && mat->cols = mat1->cols)
-    {
-        for(int i = 0; i < mat->rows; i++)
+    matrix3 = new float* [mat->rows];
+        for (int i = 0; i < mat->rows; i++)
         {
-            matrix3[i] = new float [mat->cols];
-            for(int y=0; y < mat->cols; y++)
+            matrix3[i] = new float[mat->cols];
+            for (int y = 0; y < mat->cols; y++)
             {
-                matrix3 [i] [y] = mat1 [i] [y] - mat2 [i] [y];
+                matrix3[i][y] = mat1[i][y] - mat2[i][y];
             }
         }
-    return matrix3;
-    }
-     else()
-    {
-        cout << "I cant do that math operation with different type of matrix."; 
-    }
+        return matrix3;
 }
-float** multi(matrix *mat, float **mat1, float **mat2)
+float** multi(matrix* mat, float** mat1, float** mat2)
 {
     float** matrix3;
-    matrix3 = new float * [mat->rows];
-    if(mat->rows = mat1->rows && mat->cols = mat1->cols)
-    {
-        for(int i = 0; i < mat->rows; i++)
+    matrix3 = new float* [mat->rows];
+    
+        for (int i = 0; i < mat->rows; i++)
         {
-            matrix3[i] = new float [mat->cols];
-            for(int y=0; y < mat->cols; y++)
+            matrix3[i] = new float[mat->cols];
+            for (int y = 0; y < mat->cols; y++)
             {
-                matrix3 [i] [y] = mat1 [i] [y] * mat2 [i] [y];
+                matrix3[i][y] = mat1[i][y] * mat2[i][y];
             }
         }
-    return matrix3;
-    }
-    else()
-    {
-        cout << "I cant do that math operation with different type of matrix."; 
-    }
+        return matrix3;
 }
-float** trans(matrix *mat, float **mat1)
+float** trans(matrix* mat, float** mat1)
 {
     float** matrix3;
-    matrix3 = new float * [mat->rows];
-    for(int i = 0; i < mat->rows; i++)
+    matrix3 = new float* [mat->rows];
+    for (int i = 0; i < mat->rows; i++)
     {
-        matrix3[i] = new float [mat->cols];
-        for(int y=0; y < mat->cols; y++)
+        matrix3[i] = new float[mat->cols];
+        for (int y = 0; y < mat->cols; y++)
         {
-            matrix3 [i] [y] = mat1 [y] [i] ;
+            matrix3[i][y] = mat1[y][i];
         }
     }
     return matrix3;
@@ -128,19 +115,27 @@ float** trans(matrix *mat, float **mat1)
 
 int main()
 {
+    float x=0, y=0, z, k;
     matrix mat;
-    cout << "input number of rows and collums";
-    cin >> mat.rows >> mat.cols;
-    float **mat1=input(&mat);
-    float **mat2=input(&mat);
-    float **mat7=inputGen(&mat);
+    float** mat1 = input(&mat, &x, &y);
+    z = x;
+    k = y;
+    float** mat2 = input(&mat, &x, &y);
+    float** mat7 = inputGen(&mat);
     output(&mat, mat7);
-    float **mat3=count1(&mat, mat1, mat2);
-    output(&mat, mat3);
-    float **mat4=minus1(&mat, mat1, mat2);
-    output(&mat, mat4);
-    float **mat5=multi(&mat, mat1, mat2);
-    output(&mat, mat5);
-    float **mat6=trans(&mat, mat1);
+    float** mat6 = trans(&mat, mat1);
     output(&mat, mat6);
+    if (z == x && k == y)
+    {
+        float** mat3 = count1(&mat, mat1, mat2);
+        output(&mat, mat3);
+        float** mat4 = minus1(&mat, mat1, mat2);
+        output(&mat, mat4);
+        float** mat5 = multi(&mat, mat1, mat2);
+        output(&mat, mat5);
+    }
+    else
+    {
+        cout << "I cant do that math operation with different type of matrix.";
+    }
 }
