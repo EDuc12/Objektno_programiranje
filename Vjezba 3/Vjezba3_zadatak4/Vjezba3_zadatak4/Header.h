4535affe2f537ee24d063e65e220d3e2e2e125bd
EDuc12 <93094258+EDuc12@users.noreply.github.com>
2021-11-15 20:08:08 +0100

    Update Header.h

--- Vjezba 3/Vjezba3_zadatak4/Vjezba3_zadatak4/Header.h
+++ Vjezba 3/Vjezba3_zadatak4/Vjezba3_zadatak4/Header.h
@@ -4,6 +4,5 @@
 #include<algorithm>
 using namespace std;
 void input_vector_references_element_number(vector<int>& v, int& num_elements);
-void input_vector_references_interval_element_number(vector<int>& v, int min, int max);
 void output_vector(vector<int>& v);
 
